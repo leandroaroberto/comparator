432fcf9541aa76e6f8a7303bf489aef881918f49
Leandro Roberto <leandro.roberto@dextra-sw.com>
2022-05-13 21:18:24 -0300

    dashboard import form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                        Let's update the catalog
+                    </h2>
+                    <form action="" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group mb-4" style="max-width: 500px; margin: 0 auto;">
+                            <div>
+                                <input type="file" name="file" class="custom-file-input" id="customFile">
+                                <label class="custom-file-label" for="customFile">Choose file</label>
+                            
+                            <div>
+                                <button class="btn btn-primary">Import data</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
