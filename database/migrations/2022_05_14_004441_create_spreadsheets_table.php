7e4db51a2e90e990d54a6451507bd909ead776c8
Leandro Roberto <leandro.roberto@dextra-sw.com>
2022-05-13 23:07:47 -0300

    Spreadsheet model and migration

---
+++ database/migrations/2022_05_14_004441_create_spreadsheets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpreadsheetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spreadsheets', function (Blueprint $table) {
+            $table->id();
+            $table->string('model_name');
+            $table->string('ram');
+            $table->string('ram_type');
+            $table->string('hard_disk_storage');
+            $table->integer('hard_disk_amount');
+            $table->string('hard_disk_type');
+            $table->string('location');
+            $table->decimal('price',10,2);
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spreadsheets');
+    }
+}
