e8034a2c77dc59ac72484c51ab990f5d8200f037
Leandro Roberto <leandro.roberto@dextra-sw.com>
2022-05-15 00:52:33 -0300

    HomeController API

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Throwable;
+use Illuminate\Support\Facades\DB;
+use App\Models\Spreadsheet;
+
+class HomeController extends Controller
+{
+    
+    public function listAll(){
+        $data = Spreadsheet::where('status',1)
+            ->orderBy('model_name', 'ASC')
+            ->get();
+        return response()->json($data, 200);
+    }
+
+    public function showById($id){
+        $data = Spreadsheet::where('id',$id)
+            ->where('status',1)
+            ->get();
+        return response()->json($data, 200);
+    }
+
+    public function searchByParams(Request $request){
+        /*
+        "storage": null,
+		"ram": null,
+		"harddisk": null,
+		"location": null
+        */
+        //$data = ['not implemented yet.', $request->all()];
+       
+        $data = DB::table('spreadsheets')
+            ->where('status',1)
+            ->whereIn('ram', ['2GB', '4GB', '128GB'])
+            ->orderBy('ram','DESC')
+            ->orderBy('model_name','ASC')
+            ->get();
+
+        return response()->json($data, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SpreadsheetController;
+use App\Http\Controllers\HomeController;
 
+//dashboard
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
 });
-
 Route::post('upload', [SpreadsheetController::class, 'upload'])->name('upload');
+
+//User'spage
+Route::get('list', [HomeController::class, 'listAll'])->name('listall');
+Route::get('list/{id}', [HomeController::class, 'showById'])->name('showById');
+Route::post('search', [HomeController::class, 'searchByParams'])->name('search');
